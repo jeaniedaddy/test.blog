7bcead72024757730e83da26f43386370a7d00a7
steve <jeaniedaddy@gmail.com>
2019-05-14 16:25:58 -0400

    add thunk

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from "redux";
 
 const postsReducer= (posts=[], action) => {
+   if(action.type === 'FETCH_BLOGS'){
+       return action.payload;
+   }
     return posts; 
 }
 
